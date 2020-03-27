d512e21bee1a91c068d522f7cfeae029d6a06455
linjujiu <linjujiu@gdqunxin.com>
2020-03-27 11:38:11 +0800

    创建socket套接字

--- Socket/Program.cs
+++ Socket/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
-namespace Socket
+namespace SocketProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,
+                ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any,9999);
+            socket.Bind(endPoint);
         }
     }
 }
